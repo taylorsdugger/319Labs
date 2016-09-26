513362fc62b62335d4c82aed042c6fd407c62a98
jkuczek15 <joe.kuczek@gmail.com>
2016-09-25 19:26:58 -0500

    Added form where user can select number of rows and columns.

--- Portfolio/Connect4.js
+++ Portfolio/Connect4.js
@@ -1,4 +1,4 @@
-var Game = {
+Game = {
   current_player_color: 1,
   rows: 6,
   columns: 7,
@@ -48,6 +48,11 @@ var Game = {
     // for each table cell element, attach a click listener for dropping tokens
     $("td").each(function() {
         var id = $(this).attr("id");
+
+        if(id == 0 || id == 1 || id == 2 || id == 3 || id == 4){
+          return;
+        }// end if this td element isnt apart of our connect4 game, return
+
         // onclick listener
         var board_piece = document.getElementById(id);
         board_piece.addEventListener("click", drop_token);
@@ -59,6 +64,13 @@ var Game = {
     document.getElementById('reset').addEventListener("click", function(){
         Game.init_board();
         Game.draw_board();
+    });// end of function for resetting the game
+
+    // attach a listener to our reset button to restart the game
+    document.getElementById('back').addEventListener("click", function(){
+        document.getElementById('game_space').innerHTML = '';
+        document.getElementById('game_content').innerHTML = '';
+        document.getElementById('connect4form').style.display = "block";
     });// end of function for resetting the game
   },// end function for attaching button listeners to DOM elements
 
@@ -127,7 +139,56 @@ var Game = {
           }// end for loop over columns
       }// end for loop over rows
       return win;
-  }// end of function for checking if there is a win
+  },// end of function for checking if there is a win
+
+  create_table : function(){
+    // function for generating the html table based on the game board
+    // this allows for a custom number of rows and columns to be set
+    if(Number.isInteger(parseInt(document.getElementById('rows').value), 10)){
+        Game.rows = document.getElementById('rows').value;
+    }else{
+        Game.rows = 6; // default to 6
+    }// end if argument is integer
+
+    if(Number.isInteger(parseInt(document.getElementById('columns').value), 10)){
+        Game.columns = document.getElementById('columns').value;
+    }else{
+        Game.columns = 7; // default to 7
+    }// end if argument is integer
+
+    content_string = "<h3 style='text-align: center;' id='game_text'></h3><center><button class='button' id='reset'>Restart</button><br><br><button class='button' id='back'>Back to Form</button></center>";
+    document.getElementById('game_content').innerHTML = content_string;
+
+    table_string = "<table class='table' cellspacing='0' cellpadding='0' width='774px' height='100px'>";
+    table_string += "<tr>";
+
+    for(j = 0; j < this.columns; j++){
+      table_string += "<td id=h" + j + "></td>";
+    }// end for loop over number of columns
+
+    table_string += "</tr>";
+    table_string += "<table  style='border:3px solid #000;' class='table' cellspacing='0' cellpadding='0' width='634px' height='660px'>";
+
+    for(i = 0; i < this.rows; i++){
+       table_string += '<tr>';
+
+       for(j = 0; j < this.columns; j++){
+          table_string += '<td id=' + i.toString() + j.toString() + '></td>';
+       }// end for loop over all columns
+
+       table_string += '</tr>';
+    }// end for loop over all rows
+    table_string += '</table>';
+    // set the table string in the DOM
+    document.getElementById('game_space').innerHTML = table_string;
+
+    // also attach event listeners and initalize game variables
+    Game.init_board();
+    Game.draw_board();
+    Game.attach_event_listeners();
+    document.getElementById('connect4form').style.display = "none";
+    return false;
+  }// end function for creating the table in the DOM
 };// end of object Game
 
 function drop_token(){
@@ -204,7 +265,3 @@ function top_display(){
      document.getElementById(id).className = "red_display";
    }// end if selecting the player color for mouseover
 }// end function for displaying tokens on top of the board
-
-Game.init_board();
-Game.draw_board();
-Game.attach_event_listeners();
